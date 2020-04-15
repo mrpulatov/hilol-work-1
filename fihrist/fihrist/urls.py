d6f8dd9997c957bf5d23977f045609b6fa7a1394
Pulatov <pulatovubaydulla@gmail.com>
2020-04-15 16:47:36 +0500

    Adding templates step 5(fixing problem with include function)

--- fihrist/fihrist/urls.py
+++ fihrist/fihrist/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(Letters.urls)), #letters
-    path('words/', include(Words.urls)), #words
-    path('places/', include(Places.urls)), #places
+    path('', include('Letters.urls')), #letters
+    path('words/', include('Words.urls')), #words
+    path('places/', include('Places.urls')), #places
 ]
