9a68ec4f5f84a8fbd489732fc176c928c8c46f2a
Pulatov <pulatovubaydulla@gmail.com>
2020-04-17 11:34:28 +0500

    Add Postgresql settings

--- fihrist/fihrist/settings.py
+++ fihrist/fihrist/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'fihrist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fihristi',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '192.168.1.222',
+        'PORT': '5432',
     }
 }
 
